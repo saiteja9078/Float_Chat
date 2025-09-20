c246718c7eac2c38c9cf73104506ff196e4ffa5b
saiteja9078 <149373284+saiteja9078@users.noreply.github.com>
2025-09-21 01:28:58 +0530

    Update Main_Agent.py

--- AGENTS/Main_Agent.py
+++ AGENTS/Main_Agent.py
@@ -2,26 +2,32 @@ import json
 from filter_agent import ArgoFloatAgent
 from sql_agent import run_agent
 from langgraph.graph import StateGraph, END
-from typing import TypedDict, Dict, List
+from typing import TypedDict, List
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.prompts import ChatPromptTemplate
 from pydantic import BaseModel, Field
+from sort_json import sort_cycles_in_floats
+
+
 
 class State(TypedDict):
     user_query: str
     dec_queries: List[str]
     data: dict
 
+
 model = ChatGoogleGenerativeAI(
     model="gemini-2.5-flash",
     api_key="AIzaSyA8YxYWhMe_nC2N1IHR065TWN_yHiSptAM"
 )
 
+
 class Decomposed_Queries(BaseModel):
     for_filter_agent: str = Field(description="This variable is for Filter Agent.")
     for_sql_agent: str = Field(description="This is for SQL Agent.")
 
-def decompose_query(state: State):
+
+def decompose_query(state: State) -> State:
     promts = ChatPromptTemplate.from_messages(
         [
             (
@@ -40,25 +46,31 @@ def decompose_query(state: State):
     )
     prompt = promts.format(query=state["user_query"])
     response = model.with_structured_output(Decomposed_Queries).invoke(prompt)
-    state["dec_queries"] = [response.for_filter_agent, response.for_sql_agent]
-    return state
+    return {
+        **state,
+        "dec_queries": [response.for_filter_agent, response.for_sql_agent],
+    }
 
-def execute_queries(state: State):
+
+def execute_queries(state: State) -> State:
     argo_agent = ArgoFloatAgent("AIzaSyA8YxYWhMe_nC2N1IHR065TWN_yHiSptAM")
     filtered = argo_agent.query(state["dec_queries"][0])
     results = run_agent(state["dec_queries"][1], filtered)
-    with open("results.json", "w") as f:
-        f.write(json.dumps(results, indent=2, default=str))
-    state["data"] = results
-    return state
 
+    # with open("re.txt", "a") as f:
+    #     f.write(str(state) + "\n\n")
+    # with open("results.json", "w") as f:
+    #     f.write(json.dumps(results, indent=2, default=str))
+
+    return {**state, "data": results}
 
 
 state: State = {
-    "user_query": "Get the adjusted salinity profiles for floats in Bay of Bengal",
+    "user_query": "Compare temperatures of any two floats in Indian ocean",
     "dec_queries": [],
-    "data": {}
+    "data": {},
 }
+
 workflow = StateGraph(State)
 workflow.add_node("decompose_query", decompose_query)
 workflow.add_node("execute_queries", execute_queries)
@@ -67,6 +79,8 @@ workflow.add_edge("execute_queries", END)
 workflow.set_entry_point("decompose_query")
 graph = workflow.compile()
 
-
 final_state = graph.invoke(state)
 print(final_state)
+sorted_data = sort_cycles_in_floats(final_state["data"])
+with open("results.json" ,"w") as f:
+    f.write(json.dumps(sorted_data),indent=2,default=str)
